52d21899e4721ee5d853f372b0fa23c2300e92cc
johnne <john.sundh@scilifelab.se>
2020-08-27 09:43:50 +0200

    Remove single-end support from assembly_utils.py

--- workflow/scripts/assembly_utils.py
+++ workflow/scripts/assembly_utils.py
@@ -12,7 +12,7 @@ def metaspades_input(sm):
     :return:
     """
     from common import rename_records
-    files = {"R1": [], "R2": [], "se": []}
+    files = {"R1": [], "R2": []}
     assembly_dict = sm.params.assembly
     # Collect all files belonging to the assembly group
     for sample in assembly_dict.keys():
@@ -21,21 +21,15 @@ def metaspades_input(sm):
                 files[pair].append(
                     assembly_dict[sample][unit][pair][0])
     # Rename and concatenate reads (required for Metaspades)
-    with open(sm.output.R1, 'w') as fh1, open(sm.output.R2, 'w') as fh2, open(
-        sm.output.se, 'w') as fhse:
-        i = 0
-        for f in files["R1"]:
+    with open(sm.output.R1, 'w') as fh1, open(sm.output.R2, 'w') as fh2:
+        for i, f in enumerate(files["R1"]):
             f2 = files["R2"][i]
             fh1 = rename_records(f, fh1, i)
             fh2 = rename_records(f2, fh2, i)
-            i += 1
-        for i, f in enumerate(files["se"], start=i):
-            fhse = rename_records(f, fhse, i)
 
 
 def main(sm):
-    toolbox = {"assembly_stats": stats,
-               "generate_metaspades_input": metaspades_input}
+    toolbox = {"generate_metaspades_input": metaspades_input}
     toolbox[sm.rule](sm)
 
 
